dfab92ec13c8de09c77cd2d8d40e1bef42bde417
huangfeng <huangfeng@gozap.com>
2015-04-01 15:28:19 +0800

    Add example

--- FINCameraExample/AppDelegate.m
+++ FINCameraExample/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,7 +18,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window=[[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
-    self.window.backgroundColor=[UIColor whiteColor];
+    self.window.rootViewController=[[UINavigationController alloc]initWithRootViewController:[ViewController new]];
+    
     [self.window makeKeyAndVisible];
     return YES;
 }
--- FINCameraExample/ViewController.m
+++ FINCameraExample/ViewController.m
@@ -16,7 +16,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.navigationController.navigationBarHidden=YES;
+    self.view.backgroundColor=[UIColor whiteColor];
 }
 
 - (void)didReceiveMemoryWarning {
